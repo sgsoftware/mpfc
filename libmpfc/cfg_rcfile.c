456cd111c4ec8c68ad0590b95321d06f8dd714a5
sg2 <sergey.e.galanov@gmail.com>
2014-01-31 00:38:08 +0400

    Support octal and hex escape sequences in cfg file parser.

--- libmpfc/cfg_rcfile.c
+++ libmpfc/cfg_rcfile.c
@@ -47,6 +47,63 @@ static void cfg_rcfile_skip_ws( char **str )
 		(*str) ++;
 } /* End of 'cfg_rcfile_skip_ws' function */
 
+static char parse_octal( char **str )
+{
+	unsigned char res = 0;
+	for ( int i = 0; i < 3; ++i )
+	{
+		if (isdigit(**str))
+		{
+			res *= 8;
+			res += (**str) - '0';
+			(*str) ++;
+		}
+		else break;
+	}
+	return (char)res;
+}
+
+static int hex2digit( char c )
+{
+	switch (c)
+	{
+	case '0': return 0;
+	case '1': return 1;
+	case '2': return 2;
+	case '3': return 3;
+	case '4': return 4;
+	case '5': return 5;
+	case '6': return 6;
+	case '7': return 7;
+	case '8': return 8;
+	case '9': return 9;
+	case 'a': case 'A': return 10;
+	case 'b': case 'B': return 11;
+	case 'c': case 'C': return 12;
+	case 'd': case 'D': return 13;
+	case 'e': case 'E': return 14;
+	case 'f': case 'F': return 15;
+	default: return -1;
+	}
+}
+
+static char parse_hex( char **str )
+{
+	unsigned char res = 0;
+	for ( int i = 0; i < 2; ++i )
+	{
+		int d = hex2digit(**str);
+		if (d >= 0)
+		{
+			res *= 16;
+			res += d;
+			(*str) ++;
+		}
+		else break;
+	}
+	return (char)res;
+}
+
 /* Read a string entry from configuration file line */
 char *cfg_rcfile_read_str( char **str, char *token, char (*skipper)(char**) )
 {
@@ -86,32 +143,40 @@ char *cfg_rcfile_read_str( char **str, char *token, char (*skipper)(char**) )
 		ret = (char *)malloc(len + 1);
 		if (ret == NULL)
 			return strdup("");
-		for ( ; (**str) != '\"'; (*str) ++ )
+		for ( ; (**str) && (**str) != '\"'; )
 		{
 			if ((**str) != '\\')
-				ret[i ++] = **str;
+				ret[i ++] = *(*str)++;
 			else
 			{
 				(*str) ++;
 				switch (**str)
 				{
 				case 'n':
-					ret[i ++] = '\n';
+					ret[i ++] = '\n'; (*str)++;
 					break;
 				case 't':
-					ret[i ++] = '\t';
+					ret[i ++] = '\t'; (*str)++;
 					break;
 				case '\"':
-					ret[i ++] = '\"';
+					ret[i ++] = '\"'; (*str)++;
 					break;
 				case '\\':
-					ret[i ++] = '\\';
+					ret[i ++] = '\\'; (*str)++;
 					break;
 				case 'e':
-					ret[i ++] = '\033';
+					ret[i ++] = '\033'; (*str)++;
 					break;
 				default:
-					ret[i ++] = (**str);
+					if (isdigit(**str))
+						ret[i ++] = parse_octal(str);
+					else if ((**str) == 'x')
+					{
+						(*str) ++;
+						ret[i ++] = parse_hex(str);
+					}
+					else
+						ret[i ++] = *(*str)++;
 					break;
 				}
 			}
